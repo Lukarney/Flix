306cdab71f53f23b179d12f1497ec70be4c1c086
Luke Arney <57237540+Lukarney@users.noreply.github.com>
2021-06-23 18:58:42 -0500

    inital set up for detail view

---
+++ Flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  Flix
+//
+//  Created by Luke Arney on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
