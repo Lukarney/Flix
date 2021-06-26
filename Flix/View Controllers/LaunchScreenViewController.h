4ab222f8a16ae5cf88358fb15684621e7e2788d6
Luke Arney <57237540+Lukarney@users.noreply.github.com>
2021-06-25 20:32:23 -0500

    fixed launch screen

---
+++ Flix/View Controllers/LaunchScreenViewController.h
@@ -0,0 +1,16 @@
+//
+//  LaunchScreenViewController.h
+//  Flix
+//
+//  Created by Luke Arney on 6/25/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LaunchScreenViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
